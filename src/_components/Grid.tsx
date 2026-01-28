c4fc6918cdad95a36ffc30d2e57d38f7b89f9f55
Wesley Francescon <wfrancescons@gmail.com>
2026-01-28 00:49:31 -0300

    refactor: update Grid component to use card-grid class for styling

--- src/_components/Grid.tsx
+++ src/_components/Grid.tsx
@@ -5,14 +5,7 @@ type GridProps = {
 export default function Grid({ children }: GridProps) {
   return (
     <div className="py-5 flex justify-center">
-      <div className="
-          grid
-          grid-cols-[repeat(2,160px)]
-          auto-rows-[160px]
-          gap-6
-          justify-center
-          md:grid-cols-[repeat(4,160px)]
-        ">
+      <div className="card-grid">
         {children}
       </div>
     </div>
