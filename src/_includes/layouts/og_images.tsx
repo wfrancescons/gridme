fd750b662cb0309beffbe9ea490aedbff3cde72c
Wesley Francescon <wfrancescons@gmail.com>
2026-02-02 16:16:59 -0300

    feat: refactor configuration and add OpenGraph support with new layout for images

---
+++ src/_includes/layouts/og_images.tsx
@@ -0,0 +1,93 @@
+type ogImageData = Lume.Data & {
+  name: string;
+  description: string;
+  avatar: string;
+};
+
+export default async function ogImage(
+  { name, description, avatar }: ogImageData,
+) {
+  const bytes = await Deno.readFile(`${Deno.cwd()}/src/${avatar}`);
+
+  const base64 = btoa(
+    String.fromCharCode(...bytes),
+  );
+
+  const dataUrl = `data:image/jpeg;base64,${base64}`;
+
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#fafafa",
+        textAlign: "center",
+        gap: "16px",
+        paddingTop: "24px",
+        paddingBottom: "16px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          width: "104px", // size-26 (26 * 4px)
+          height: "104px",
+          borderRadius: "9999px",
+          borderWidth: "4px",
+          borderStyle: "solid",
+          borderColor: "#ffffff",
+          boxShadow: "0 10px 15px rgba(0,0,0,0.1)", // shadow-lg/10
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src={dataUrl}
+          alt="Avatar"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        />
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "8px",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "30px", // text-3xl
+            lineHeight: "36px",
+            fontWeight: 700,
+            color: "#171717", // neutral-900
+            margin: 0,
+          }}
+        >
+          {name}
+        </h1>
+
+        <p
+          style={{
+            fontSize: "16px",
+            lineHeight: "24px",
+            color: "#737373", // neutral-500
+            maxWidth: "384px", // max-w-sm
+            margin: 0,
+          }}
+        >
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+}
