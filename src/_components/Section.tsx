3408c83d01e7634ae212e0f1d670f4fb72c519ed
Wesley Francescon <wfrancescons@gmail.com>
2026-02-04 15:36:50 -0300

    style: change subtitle gap

--- src/_components/Section.tsx
+++ src/_components/Section.tsx
@@ -5,7 +5,7 @@ export type SectionProps = {
 
 export default function Section({ title, subtitle }: SectionProps) {
   return (
-    <div class="flex flex-col gap-2 items-center text-center">
+    <div class="flex flex-col gap-1 items-center text-center">
       <h2 class="text-2xl font-bold text-neutral-800">{title}</h2>
       {subtitle && <p class="text-sm text-neutral-500">{subtitle}</p>}
     </div>
