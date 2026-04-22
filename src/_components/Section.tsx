adf03ccbadf965e054fce69f0bb9e8a397387f60
Wesley Francescon <wfrancescons@gmail.com>
2026-04-22 00:21:36 -0300

    style: adjust text spacing in sections

--- src/_components/Section.tsx
+++ src/_components/Section.tsx
@@ -5,7 +5,7 @@ export type SectionProps = {
 
 export default function Section({ title, subtitle }: SectionProps) {
   return (
-    <div class="flex flex-col items-center gap-1 text-center">
+    <div class="flex flex-col items-center gap-1.5 text-center">
       <h2 class="font-bold text-2xl text-neutral-800">{title}</h2>
       {subtitle && <p class="text-neutral-500 text-sm">{subtitle}</p>}
     </div>
